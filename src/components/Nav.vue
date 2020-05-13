22b8c0135f72a76990d5e4ac5a109eb14d5ade9c
MadgeLee <le30n@outlook.com>
2020-05-13 10:38:23 +0800

    初步使用svg-sprite-loader引入icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+    记账
+    </router-link>
     <router-link to="/labels">标签</router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
