ed81fd63e192011f83b9862e68f668ad02ad051e
MadgeLee <le30n@outlook.com>
2020-05-14 13:34:49 +0800

    numberPad组件init

---
+++ src/components/money/NumberPad.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="currentNumber">0</div>
+    <div class="number-wrapper">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>X</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>C</button>
+      <button>0</button>
+      <button>.</button>
+      <button>确定</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.currentNumber {
+  text-align: right;
+  padding: 10px;
+  font-size: 20px;
+}
+.number-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  button {
+    height: 64px;
+    width: 25%;
+    border: none;
+    background: transparent;
+    $bgColor: #f2f2f2;
+    &:nth-child(1) {
+      background: $bgColor;
+    }
+    &:nth-child(2),&:nth-child(5) {
+      background: darken($bgColor,4%);
+    }
+    &:nth-child(3),&:nth-child(6),&:nth-child(9) {
+      background: darken($bgColor,4*2%);
+    }
+    &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
+        background:darken($bgColor, 4*3%)
+    }
+    &:nth-child(8),&:nth-child(11),&:nth-child(14){
+        background:darken($bgColor, 4*5%)
+    }
+    &:nth-child(12),&:nth-child(15) {
+      background: darken($bgColor,4*6%);
+    }
+    &:nth-child(16) {
+      background: darken($bgColor,4*7%);
+    }
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,6 +3,7 @@
     <Types />
     <Tags class="money-tags" />
     <Note />
+    <NumberPad />
   </div>
 </template>
 
@@ -10,24 +11,26 @@
 import Types from "@/components/money/Types.vue";
 import Tags from "@/components/money/Tags.vue";
 import Note from "@/components/money/Note.vue";
+import NumberPad from "@/components/money/NumberPad.vue";
 
 export default {
   name: "Money",
   components: {
     Types,
     Tags,
-    Note
+    Note,
+    NumberPad
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.money-warpper{
+.money-warpper {
   height: 100vh;
-  display:flex;
-  flex-direction:column;
-  .money-tags{
-    flex-grow:1;
+  display: flex;
+  flex-direction: column;
+  .money-tags {
+    flex-grow: 1;
   }
 }
 </style>
