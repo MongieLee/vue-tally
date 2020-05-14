e5b6b68485cac1efa6224a8aefe9b8b927bac46c
MadgeLee <le30n@outlook.com>
2020-05-14 13:18:02 +0800

    note组件init

---
+++ src/components/money/Note.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="note-wrapper">
+    <label for="note">备注：</label>
+    <input id="note" type="text" autocomplete="off" placeholder="点击填写备注..." />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Note"
+};
+</script>
+
+<style lang="scss" scoped>
+.note-wrapper {
+  padding: 3px 5px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  background: #f5f5f5;
+  input {
+    margin-left: 10px;
+    flex-grow: 1;
+    border: none;
+    padding: 5px 3px;
+    background: transparent;
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
+  <div class="money-warpper">
     <Types />
-    <Tags />
+    <Tags class="money-tags" />
+    <Note />
   </div>
 </template>
 
 <script lang='ts'>
 import Types from "@/components/money/Types.vue";
 import Tags from "@/components/money/Tags.vue";
+import Note from "@/components/money/Note.vue";
 
 export default {
   name: "Money",
   components: {
     Types,
-    Tags
+    Tags,
+    Note
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.money-warpper{
+  height: 100vh;
+  display:flex;
+  flex-direction:column;
+  .money-tags{
+    flex-grow:1;
+  }
+}
+</style>
