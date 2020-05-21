6a5defcb2623a6ed0c65cfb147f278363d020f28
MongieLee <mongielee@163.com>
2020-05-21 17:25:47 +0800

    提交

--- src/views/ReportForm.vue
+++ src/views/ReportForm.vue
@@ -2,30 +2,30 @@
   <Layout>
     <div class="top-bar">
       <div class="type">
-        <span :class="{active:type==='pay'}" @click="heihei('pay')">支出</span>
-        <span :class="{active:type==='income'}" @click="heihei('income')">收入</span>
+        <span :class="{active:type==='pay'}" @click="changeType('pay')">支出</span>
+        <span :class="{active:type==='income'}" @click="changeType('income')">收入</span>
       </div>
 
       <ul class="company-date">
-        <!-- <li @click="change('week')" :class="{active:companyDate==='week'}">周</li> -->
+        <li @click="change('week')" :class="{active:companyDate==='week'}">周</li>
         <li @click="change('month')" :class="{active:companyDate==='month'}">月</li>
-        <!-- <li @click="change('year')" :class="{active:companyDate==='year'}">年</li> -->
+        <li @click="change('year')" :class="{active:companyDate==='year'}">年</li>
       </ul>
     </div>
-    <div>{{getText}}</div>
-    <div>总支出：{{parseFloat(xxx).toFixed(2)}}</div>
-    <div>平均支出：{{parseFloat(xxx/31).toFixed(2)}}</div>
-    <div id="main"></div>
+    <div>{{getTimeText}}</div>
+    <div>总支出：{{parseFloat(totalAmount).toFixed(2)}}</div>
+    <div>平均支出：{{parseFloat(totalAmount/31).toFixed(2)}}</div>
+    <div id="lineChart"></div>
     <div>
-      <span>消费占比</span>
-      <div id="ppp" class="aaa"></div>
+      <div id="pieChart" class="aaa"></div>
     </div>
-    {{getBing}}
   </Layout>
 </template>
 
 <script>
 import dayJs from "dayjs";
+import myChart from "@/lib/echarts.js";
+
 export default {
   name: "ReportForm",
   data() {
@@ -33,278 +33,157 @@ export default {
       type: "pay",
       companyDate: "month",
       currentList: [],
-      xxx: 0,
-      pingList: [] //[{value:1,name:'qqq'}]
+      totalAmount: 0,
+      lineData: [],
+      pieData: [] //[{value:1,name:'qqq'}]
     };
   },
   methods: {
-    heihei(s) {
-      this.type = s;
-      var myChart = this.$echarts.init(document.getElementById("main"));
-      // 绘制图表
-      myChart.setOption(
-        {
-          title: {},
-          tooltip: {
-            trigger: "axis",
-            axisPointer: {
-              animation: false
-            }
-          },
-          xAxis: {
-            type: "category",
-            boundaryGap: false,
-            axisLine: { onZero: true },
-            data: [
-              "1",
-              "2",
-              "3",
-              "4",
-              "5",
-              "6",
-              "7",
-              "8",
-              "9",
-              "10",
-              "11",
-              "12",
-              "13",
-              "14",
-              "15",
-              "16",
-              "17",
-              "18",
-              "19",
-              "20",
-              "21",
-              "22",
-              "23",
-              "24",
-              "25",
-              "26",
-              "27",
-              "28",
-              "29",
-              "30",
-              "31"
-            ]
-          },
-          yAxis: {
-            show: false
-          },
-          series: [{ name: "收入", data: this.getCurrentList, type: "line" }]
-        },
-        true
+    changeType(type) {
+      this.type = type;
+      this.getLineData()
+      myChart.createLineChart(
+        "lineChart",
+        this.companyDate,
+        this.lineData
       );
     },
     change(string) {
       this.companyDate = string;
+    },
+    getLineData() {
+      let payOrIncomeList = []; //记录最终所有支出or收入金额结果数组
+      this.totalAmount = 0;
+      let type = this.type;
+      console.log("i run ");
+      const nullRecordObj = {
+        week: [0, 0, 0, 0, 0, 0, 0],
+        month: [
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0
+        ],
+        year: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      };
+      let allRecord = JSON.parse(JSON.stringify(this.recordList)); //获取所有账单数据
+      if (allRecord.length === 0) {
+        return nullRecordObj[this.companyDate];
+      }
+      let newArr = [];
+
+      const handleTypeList = {
+        week() {},
+        month: () => {
+          console.log("i run2 ");
+
+          allRecord.map(v => {
+            dayJs(v.createTime).month() === dayJs(new Date()).month() &&
+              newArr.push(v);
+          }); //筛选出账单类别中所有属于本月的账单
+
+          for (let i = 0; i < 31; i++) {
+            newArr.map(v => {
+              //得到本月31天每一天的账单数据
+              if (!payOrIncomeList[i]) {
+                payOrIncomeList[i] = [];
+              }
+              if (dayJs(v.createTime).date() === i + 1) {
+                payOrIncomeList[i].push(v);
+              }
+            });
+          }
+          payOrIncomeList.map((v, index) => {
+            //31天每一天的总金额
+            if (v.length === 0) {
+              payOrIncomeList[index] = 0; //没有数据则当天为0
+            } else {
+              let count = 0;
+              v.map(value => {
+                //遍历有数据的那天
+                if (value.type === type) {
+                  count += parseFloat(value.amount);
+                }
+              });
+              payOrIncomeList[index] = count;
+            }
+          });
+          payOrIncomeList.map(value => {
+            this.totalAmount += value;
+          });
+          this.lineData = payOrIncomeList;
+        },
+        year() {}
+      };
+      handleTypeList[this.companyDate]()
+    },
+    getPieData() {
+      let allRecord = JSON.parse(JSON.stringify(this.recordList));
+      if (allRecord.length === 0) {
+        this.pieData =  [{ value: 0, name: "暂无数据" }];
+      }
+      let dataObj = {};
+      allRecord.map(v => {
+        let type = v.selectedTag.tagType;
+        if (!dataObj[type]) {
+          dataObj[type] = v.amount;
+        } else {
+          dataObj[type] += parseFloat(v.amount);
+        }
+      });
+      let chartData = [];
+      for (let key in dataObj) {
+        chartData.push({ value: dataObj[key], name: key });
+      }
+      this.pieData =  chartData;
     }
   },
   computed: {
-    getText() {
-      let o = {
+    getTimeText() {
+      let timeMap = {
         week: "本周",
         month: "本月",
         year: "今年"
       };
-      return o[this.companyDate];
+      return timeMap[this.companyDate];
     },
     recordList() {
       return this.$store.state.recordList;
     },
-    getCurrentList() {
-      this.xxx = 0;
-      let type = this.type; //获取统计类别
-      console.log(this.type);
-      let m = JSON.parse(JSON.stringify(this.recordList)); //获取所有账单数据
 
-      let o = []; //最终支出||收入金额结果数组
-      let newArr = [];
-      if (m.length === 0) {
-        for (let i = 0; i < 31; i++) {
-          o[i] = 0;
-        }
-        return o; //如果没有数据则返回空的数据
-      }
-
-      if (this.companyDate === "month") {
-        m.map(v => {
-          dayJs(v.createTime).month() === dayJs(new Date()).month() &&
-            newArr.push(v);
-        }); //筛选出账单类别中所有属于本月的账单
-
-        for (let i = 0; i < 31; i++) {
-          newArr.map(v => {
-            //得到本月31天每一天的账单数据
-            if (!o[i]) {
-              o[i] = [];
-            }
-            if (dayJs(v.createTime).date() === i + 1) {
-              o[i].push(v);
-            }
-          });
-        }
-        o.map((v, index) => {
-          //31天每一天的总金额
-          if (v.length === 0) {
-            o[index] = 0;
-          } else {
-            let count = 0;
-            v.map(q => {
-              if (q.type === this.type) {
-                count += parseFloat(q.amount);
-              }
-            });
-            o[index] = count;
-          }
-        });
-        console.log(o);
-        o.map(a => {
-          this.xxx += a;
-        });
-      }
-      return o;
-    },
-    getBing() {
-      console.log("1111111111111111");
-      let o = JSON.parse(JSON.stringify(this.recordList));
-      if (o.length === 0) {
-        return [{ value: 0, name: "暂无数据" }];
-      }
-      let q = {};
-      o.map((v, index) => {
-        let leixing = v.selectedTag.tagType;
-        if (!q[leixing]) {
-          q[leixing] = v.amount;
-        } else {
-          q[leixing] += parseFloat(v.amount);
-        }
-      });
-      console.log("zeshiq:", q);
-      let i = [];
-      for (let key in q) {
-        i.push({ value: q[key], name: key });
-      }
-      console.log(i);
-      return i;
-    }
+    
   },
   mounted() {
-    var myChart = this.$echarts.init(document.getElementById("main"));
-    // 绘制图表
-    myChart.setOption({
-      title: {},
-      tooltip: {
-        trigger: "axis",
-        axisPointer: {
-          animation: false
-        }
-      },
-      xAxis: {
-        type: "category",
-        boundaryGap: false,
-        axisLine: { onZero: true },
-        data: [
-          "1",
-          "2",
-          "3",
-          "4",
-          "5",
-          "6",
-          "7",
-          "8",
-          "9",
-          "10",
-          "11",
-          "12",
-          "13",
-          "14",
-          "15",
-          "16",
-          "17",
-          "18",
-          "19",
-          "20",
-          "21",
-          "22",
-          "23",
-          "24",
-          "25",
-          "26",
-          "27",
-          "28",
-          "29",
-          "30",
-          "31"
-        ]
-      },
-      yAxis: {
-        show: false
-      },
-      series: [{ name: "支出", data: this.getCurrentList, type: "line" }]
-    });
-  console.log(this.getBing)
-    //////////////////////////////////////////////
-    var myChart2 = this.$echarts.init(document.getElementById("ppp"));
-    myChart2.setOption({
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-      },
-
-      visualMap: {
-        show: false,
-        min: 80,
-        max: 600,
-        inRange: {
-          colorLightness: [0, 1]
-        }
-      },
-      series: [
-        {
-          backgroundColor: '#2c343c',
-          name: "消费类型",
-          type: "pie",
-          radius: "55%",
-          center: ["50%", "50%"],
-          data:
-            //  this.getBing,
-            [
-              { value: 334, name: "草拟" },
-              { value: 222, name: "妈妈" }
-            ],
-          // [
-          //   { value: 335, name: "直接访问" },
-          //   { value: 310, name: "邮件营销" }
-          // ],
-          // .sort(function(a, b) {
-          //   return a.value - b.value;
-          // }),
-          roseType: "radius",
-          label: {
-            color: "rgba(255, 255, 255, 0.3)"
-          },
-          labelLine: {
-            lineStyle: {
-              color: "rgba(255, 255, 255, 0.3)"
-            },
-            smooth: 0.2,
-            length: 10,
-            length2: 20
-          },
-          itemStyle: {
-            color: "#c23531"
-          },
-
-          animationType: "scale",
-          animationEasing: "elasticOut",
-          animationDelay: function(idx) {
-            return Math.random() * 200;
-          }
-        }
-      ]
-    });
+    this.getLineData();
+    myChart.createLineChart("lineChart", this.companyDate, this.lineData);
+    myChart.createPieChart("pieChart", this.pieData);
   },
 
   created() {
@@ -331,14 +210,13 @@ $base-color: rgb(255, 218, 71);
     }
   }
 }
-#main {
+#lineChart {
   width: 100vw;
   height: 17vh;
 }
-.aaa {
+#pieChart {
   width: 100vw;
   height: 50vh;
-  margin-top: -5vh;
 }
 .company-date {
   display: flex;
